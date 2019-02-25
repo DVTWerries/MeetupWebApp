2150bfc43168cb290868c5c6c37053d9d86f7001
Werner Buys <WBuys@jhb.dvt.co.za>
2019-02-25 16:00:19 +0200

    cleaned up the jacascript code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,10 @@
 import '../styles/index.scss';
 import 'bootstrap';
+import * as $ from 'jquery';
 
-var $ = require('jquery');
-
-console.log('webpack starterkit');
-var apiKey = '1345494f356d223964372b57807f1f79';
-var baseURl = 'https://api.meetup.com';
-var herokuAppURL = 'https://cors-anywhere.herokuapp.com/';
+const apiKey = '1345494f356d223964372b57807f1f79';
+const baseURl = 'https://api.meetup.com';
+const herokuAppURL = 'https://cors-anywhere.herokuapp.com/';
 
  $(function () {
     var $carousal = $('.carousel-inner');
@@ -32,32 +30,21 @@ $(function () {
         type: 'GET',
         url: `${herokuAppURL}${baseURl}/find/groups?key=${apiKey}`,
         success: function (data) {
-            console.log(data);
-            $.each(data, function (i = 0, item) {
+            $.each(data, function (i, item) {
+                let imageUrl = (item.group_photo || 
+                    item.key_photo ||
+                    item.meta_category.photo || {}).photo_link;
+                    console.log(imageUrl);
                 $card.append(`
                 <div class="card col-sm-4 col-md-3 col-lg-2 mx-2">
-                    <img class="card-img-top" src="${ typeof item.group_photo !== "undefined" ? item.group_photo.photo_link 
-                            : typeof  item.key_photo !== "undefined" ? item.key_photo.photo_link : item.meta_category.photo.photo_link}" alt="Card image cap">
+                    <img class="card-img-top" src="${imageUrl}"
                     <div class="card-body">
                         <h5 class="card-title">${item.name}</h5>
-                        <p class="card-text"></p>
+                        <p class="card-text">${item.description}</p>
                     </div>
                 </div>`
                 );
-                i++;
             });
         }
     });
-});
-
-$('.card').on('click', function () {
-    alert("hello");
-});
-
-$('.category').on('click', function () {
-    alert("hello");
-});
-
-$('.category').on('click', function () {
-    alert("hello");
 });
