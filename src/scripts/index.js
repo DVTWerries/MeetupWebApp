e446f9a80d52e542d499336385d7aed4ac6d1e7c
Werner Buys <WBuys@jhb.dvt.co.za>
2019-02-25 11:33:07 +0200

    Add pagination

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,9 +7,13 @@ console.log('webpack starterkit');
 var apiKey = '1345494f356d223964372b57807f1f79';
 var baseURl = 'https://api.meetup.com';
 var herokuAppURL = 'https://cors-anywhere.herokuapp.com/';
+var pageItemCount;
+var offset = 20;
+var groupData;
 
- $(function () {
+$(function () {
     var $carousal = $('.carousel-inner');
+
     $.ajax({
         type: 'GET',
         url: `${herokuAppURL}${baseURl}/2/topic_categories?key=${apiKey}`,
@@ -17,6 +21,7 @@ var herokuAppURL = 'https://cors-anywhere.herokuapp.com/';
             $.each(data.results, function (i, item) {
                 $carousal.append(`
                     <div class="carousel-item ${i == 0 ? 'active' : ''} container">
+                        <h3>${item.name}</h3>
                         <img src="${item.icon.photo_link}" alt="Another slide"></div>
                     </div>`
                 );
@@ -25,36 +30,80 @@ var herokuAppURL = 'https://cors-anywhere.herokuapp.com/';
     });
 });
 
-$(function () {
-    var $card = $('.card-holder');
+$(function() {
+    GetAllGroups();
+} );
 
+var GetAllGroups = function () {
+    
     $.ajax({
         type: 'GET',
         url: `${herokuAppURL}${baseURl}/find/groups?key=${apiKey}`,
-        success: function (data) {
-            console.log(data);
-            $.each(data, function (i = 0, item) {
-                $card.append(`
-                <div class="card col-sm-4 col-md-3 col-lg-2 mx-2">
-                    <img class="card-img-top" src="${ typeof item.group_photo !== "undefined" ? item.group_photo.photo_link 
-                            : typeof  item.key_photo !== "undefined" ? item.key_photo.photo_link : item.meta_category.photo.photo_link}" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">${item.name}</h5>
-                        <p class="card-text"></p>
-                    </div>
-                </div>`
-                );
-                i++;
-            });
+        success: function(data) {
+            groupData = data;
+            Pagination(groupData);
+            DisplayGroupCards(groupData, 0);
         }
     });
-});
+};
+
+function DisplayGroupCards(data, value) {
+    var $card = $('.card-holder');
+
+    $.each(data, (i, item) => {
+        let imageUrl = 
+            (item.group_photo ||
+                item.key_photo || 
+                item.meta_category.photo || 
+                { }).photo_link;
+
+        if (value >= offset) {
+            return;
+        }
+
+        $card.append(`
+            <div class="card col-sm-4 col-md-3 col-lg-2 mx-2">
+                <img class="card-img-top" src="${ typeof item.group_photo !== "undefined" ? item.group_photo.photo_link 
+                        : typeof  item.key_photo !== "undefined" ? item.key_photo.photo_link : item.meta_category.photo.photo_link}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${item.name}</h5>
+                    <p class="card-text">${item.description}</p>
+                </div>
+            </div>`
+        );
+
+        value++;
+    });
+}
+
+function Pagination(data) {
+    var $pagination = $('.pagination');
+    pageItemCount = data.length / 20;
+
+    for (var i = 0; i < pageItemCount ; i++) {
+        $pagination.append(
+            `<li id="${i}" class="page-item"><a class="page-link">${i}</a></li>`);
+    }
+
+    var pageItems = document.getElementsByClassName("page-item");
+
+    for (var i = 0; i < pageItems.length; i++) {
+        pageItems[i].addEventListener("click", PaginationResponse, false);
+    }
+}
+
+function PaginationResponse() {
+    var elementValue = document.getElementsByClassName("page-link").value;
+    console.log(elementValue);
+
+    for (var i = 0; i < pageItemCount; i++) {
+        if (elementValue == i) {
+            DisplayGroupCards(groupData, elementValue * 20);
+        }
+    }
+};
 
 $('.card').on('click', function () {
-    alert("hello");
-});
-
-$('.category').on('click', function () {
     alert("hello");
 });
 
