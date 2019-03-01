5140faeb89990616d8e5fce9dea189d4fcfdedd3
Werner Buys <WBuys@jhb.dvt.co.za>
2019-03-01 11:55:07 +0200

    added html, styles and background image

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,9 +6,10 @@ const apiKey = '1345494f356d223964372b57807f1f79';
 const baseURl = 'https://api.meetup.com';
 const herokuAppURL = 'https://cors-anywhere.herokuapp.com/';
 let $body = $('.body');
+let $container = $('.container-fluid');
 
 $(function () {
-    let $carousal = $('.carousel-inner');
+    let $listGroup = $('.list-group');
     $.ajax({
         type: 'GET',
         url: `${herokuAppURL}${baseURl}/2/topic_categories?key=${apiKey}`,
@@ -18,28 +19,28 @@ $(function () {
                     <span class="sr-only">Loading...</span>
                 </div>
             `);
+            $container.addClass(".loading-screen");
         },
         success: function (data) {
             $.each(data.results, function (i, item) {
-                $carousal.append(`
-                    <div class="carousel-item ${i == 0 ? 'active' : ''} container">
-                        <img src="${item.icon.photo_link}" alt="Another slide"></div>
-                    </div>
+                $listGroup.append(`
+                    <a href="#" class="list-group-item list-group-item-action bg-light loading">${item.name}</a>
                 `);
             });
         },
         error: function () {
             $body.append(`
-            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
+                            ...
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -54,7 +55,7 @@ $(function () {
 });
 
 $(function () {
-    let $cardGroup = $('.card-group');
+    let $cardHolder = $('.card-holder');
 
     $.ajax({
         type: 'GET',
@@ -64,16 +65,17 @@ $(function () {
                 let imageUrl = (item.group_photo ||
                     item.key_photo ||
                     item.meta_category.photo || {}).photo_link;
-                $cardGroup.append(`
-                    <div class="card col-lg-3 col-md-4 col-sm-6" >
-                        <img src="${imageUrl}" class="card-img-top" alt="...">
-                        <h5 class="card-title">${item.name}</h5>
-                        <div class="card-footer">
-                            <a href="#" class="btn btn-primary btn-block">View</a>
-                        </div>
+                $cardHolder.append(`
+                    <div class="col-lg-4 col-md-4 col-6 loading">
+                        <a href="http://localhost:8080/details.html?${item.urlname}" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="${imageUrl}" alt="">
+                        </a>
                     </div>
                 `);
             });
+            $container.removeClass(".loading-screen");
+            $container.css("display", "block");
+            $(".spinner-border").remove();
 
         }, 
         error: function () {
@@ -82,12 +84,13 @@ $(function () {
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
+                            ...
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -97,6 +100,7 @@ $(function () {
                     </div>
                 </div>
             `);
+            $('#exampleModalCenter').modal('show');
         }
     });
 
