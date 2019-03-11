da8afc83bff11ee280347abeaadeb50be67566da
Werner Buys <WBuys@jhb.dvt.co.za>
2019-03-11 17:27:09 +0200

    added more components and a caroursel type feature

---
+++ src/components/group-description.js
@@ -0,0 +1,42 @@
+export default class GroupDescription extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    get description() {
+        this.getAttribute('description');
+    }
+
+    set description(description) {
+        this.setAttribute('description', description);
+    }
+
+    static get observedAttributes() {
+        return ['description'];
+    }
+
+    connectedCallback() {
+        let template = `
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Description
+                            </button>
+                            </h2>
+                        </div>
+                
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                ${this.description}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+
+
+        this.innerHTML = template;
+    }
+}
--- src/scripts/details.js
+++ src/scripts/details.js
@@ -1,9 +1,12 @@
 import 'bootstrap';
 import '../styles/details.scss';
+import * as $ from 'jquery';
+
 import ErrorModal from '../components/error-modal';
 import LoadingSpinner from '../components/spinner';
+import GroupDiscription from '../components/group-description';
+import Table from '../components/table';
 
-import * as $ from 'jquery';
 
 const apiKey = '1345494f356d223964372b57807f1f79';
 const baseURl = 'https://api.meetup.com';
@@ -12,8 +15,11 @@ let $body = $('.body');
 
 customElements.define("error-modal", ErrorModal);
 customElements.define("loading-spinner", LoadingSpinner);
+customElements.define("group-discription", GroupDiscription);
+customElements.define("member-table", Table);
 
 $(function () {
+    let carousalInner = $(`.carousel-inner`);
     let urlParameter = window.location.href.split('?');
     $.ajax({
         type: 'GET',
@@ -24,7 +30,17 @@ $(function () {
             `);
         },
         success: function (data) {
-            alert(data);
+            carousalInner.append(`
+                <group-discription class="carousel-item active" description="${data.description}"></group-discription>
+                <member-table class="carousel-item"></member-table>
+                <div class="carousel-item meetup-container"></div>
+                <div class="carousel-item highligth-container"></div>
+                <div class="carousel-item" discussion-container></div>
+            `);
+            getMembers(urlParameter);
+            getMeetups(urlParameter);
+            getHighlights(urlParameter);
+            getDiscussions(urlParameter);
         },
         error: function(data) {
             $body.append(`
@@ -34,4 +50,89 @@ $(function () {
             $('#exampleModalCenter').modal('show');
         }
     });
-}, false);
+});
+
+function getMembers(urlParameter) {
+    let tableBody = $('tbody');
+    $.ajax({
+        type: 'GET',
+        url: `${herokuAppURL}${baseURl}/${urlParameter[1]}/members?key=${apiKey}`,
+        success: function (data) {
+            $.each(data, function(i, item) {
+                let imageUrl = item.photo === undefined ? "..." : item.photo.thumb_link === undefined ? item.photo.photo.link : item.photo.highres_link;
+                tableBody.append(`
+                    <tr>
+                        <th scope="row">${item.id}</th>
+                        <td><img src="${imageUrl}" width="100px" height="80px"></td>
+                        <td>${item.name}</td>
+                        <td>${item.joined}</td>
+                    </tr>
+                `);
+            });  
+        },
+    });
+}
+
+function getMeetups(urlParameter) {
+    let tableBody = $('tbody');
+    $.ajax({
+        type: 'GET',
+        url: `${herokuAppURL}${baseURl}/${urlParameter[1]}/members?key=${apiKey}`,
+        success: function (data) {
+            $.each(data, function(i, item) {
+                let imageUrl = item.photo === undefined ? "..." : item.photo.thumb_link === undefined ? item.photo.photo.link : item.photo.highres_link;
+                tableBody.append(`
+                    <tr>
+                        <th scope="row">${item.id}</th>
+                        <td></td>
+                        <td>${item.name}</td>
+                        <td>${item.joined}</td>
+                    </tr>
+                `);
+            });  
+        },
+    });
+}
+
+function getHighlights(urlParameter) {
+    let tableBody = $('tbody');
+    $.ajax({
+        type: 'GET',
+        url: `${herokuAppURL}${baseURl}/${urlParameter[1]}/members?key=${apiKey}`,
+        success: function (data) {
+            $.each(data, function(i, item) {
+                let imageUrl = item.photo === undefined ? "..." : item.photo.thumb_link === undefined ? item.photo.photo.link : item.photo.highres_link;
+                tableBody.append(`
+                    <tr>
+                        <th scope="row">${item.id}</th>
+                        <td></td>
+                        <td>${item.name}</td>
+                        <td>${item.joined}</td>
+                    </tr>
+                `);
+            });  
+        },
+    });
+}
+
+
+function getDiscussions(urlParameter) {
+    let tableBody = $('tbody');
+    $.ajax({
+        type: 'GET',
+        url: `${herokuAppURL}${baseURl}/${urlParameter[1]}/members?key=${apiKey}`,
+        success: function (data) {
+            $.each(data, function(i, item) {
+                let imageUrl = item.photo === undefined ? "..." : item.photo.thumb_link === undefined ? item.photo.photo.link : item.photo.highres_link;
+                tableBody.append(`
+                    <tr>
+                        <th scope="row">${item.id}</th>
+                        <td></td>
+                        <td>${item.name}</td>
+                        <td>${item.joined}</td>
+                    </tr>
+                `);
+            });  
+        },
+    });
+}
